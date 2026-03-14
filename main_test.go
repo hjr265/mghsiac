53c132e4036bdefd48ec778595f7461ff0611f29
Mahmud Ridwan <m@hjr265.me>
2026-03-14 16:23:58 +0600

    Fix test

--- main_test.go
+++ main_test.go
@@ -10,18 +10,18 @@ func TestTimeToEmoji(t *testing.T) {
 		in   time.Time
 		want string
 	}{
-		{time.Date(2023, 9, 8, 12, 30, 0, 0, time.UTC), "1230"},
-		{time.Date(2023, 9, 8, 12, 45, 0, 0, time.UTC), "1230"},
-		{time.Date(2023, 9, 8, 12, 59, 0, 0, time.UTC), "1230"},
-		{time.Date(2023, 9, 8, 12, 35, 0, 0, time.UTC), "1230"},
-		{time.Date(2023, 9, 8, 12, 15, 0, 0, time.UTC), "12"},
-		{time.Date(2023, 9, 8, 12, 0, 0, 0, time.UTC), "12"},
-		{time.Date(2023, 9, 8, 12, 20, 0, 0, time.UTC), "12"},
-		{time.Date(2023, 9, 8, 12, 29, 0, 0, time.UTC), "12"},
-		{time.Date(2023, 9, 8, 13, 50, 0, 0, time.UTC), "130"},
-		{time.Date(2023, 9, 8, 15, 30, 0, 0, time.UTC), "330"},
-		{time.Date(2023, 9, 8, 18, 20, 0, 0, time.UTC), "6"},
-		{time.Date(2023, 9, 8, 2, 29, 0, 0, time.UTC), "2"},
+		{time.Date(2023, 9, 8, 12, 30, 0, 0, time.UTC), ":clock1230:"},
+		{time.Date(2023, 9, 8, 12, 45, 0, 0, time.UTC), ":clock1230:"},
+		{time.Date(2023, 9, 8, 12, 59, 0, 0, time.UTC), ":clock1:"},
+		{time.Date(2023, 9, 8, 12, 35, 0, 0, time.UTC), ":clock1230:"},
+		{time.Date(2023, 9, 8, 12, 15, 0, 0, time.UTC), ":clock12:"},
+		{time.Date(2023, 9, 8, 12, 0, 0, 0, time.UTC), ":clock12:"},
+		{time.Date(2023, 9, 8, 12, 20, 0, 0, time.UTC), ":clock1230:"},
+		{time.Date(2023, 9, 8, 12, 29, 0, 0, time.UTC), ":clock1230:"},
+		{time.Date(2023, 9, 8, 13, 50, 0, 0, time.UTC), ":clock2:"},
+		{time.Date(2023, 9, 8, 15, 30, 0, 0, time.UTC), ":clock330:"},
+		{time.Date(2023, 9, 8, 18, 20, 0, 0, time.UTC), ":clock630:"},
+		{time.Date(2023, 9, 8, 2, 29, 0, 0, time.UTC), ":clock230:"},
 	} {
 		t.Run(c.in.String(), func(t *testing.T) {
 			got := timeToEmoji(c.in)
